47e5b85bfb20aded96e794ed519b1765674958d7
Teemu Kallio <teemu.kallio@dka.io>
2019-12-08 12:19:42 +0100

    clean ups

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 import style from './App.module.scss';
 
-const App: React.FC = () => {
-  return (
-    <div className={style.app}>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className={style.app}>
+  </div>
+);
 
 export default App;
