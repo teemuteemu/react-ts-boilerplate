df702eeac8099be5131a66b323ef4f4a7686ddaa
Teemu Kallio <teemu.kallio@dka.io>
2019-12-08 12:15:26 +0100

    init

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import style from './App.module.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className={style.app}>
+    </div>
+  );
+}
+
+export default App;
